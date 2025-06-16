46c51e30738ef5258e7698e7f61564e67c887dc5
Paus <cotnepau@gmail.com>
2025-06-16 15:08:48 -0600

    Update index.js

--- Javascript-The-Definitive-Guide/ch-6/src/index.js
+++ Javascript-The-Definitive-Guide/ch-6/src/index.js
@@ -1,1 +1,41 @@
+let persona = {
+    nombre: "Ana",
+    apellido: "Martínez",
+    edad: 30,
 
+    get nombreCompleto() {
+        return `${this.nombre} ${this.apellido}`;
+    },
+    set nombreCompleto(valor) {
+        const partes = valor.split(" ");
+        this.nombre = partes[0];
+        this.apellido = partes[1];
+    },
+
+    saludar() {
+        return `Hola, soy ${this.nombre}`;
+    }
+};
+
+console.log(persona.saludar());              
+console.log(persona.nombreCompleto);         
+persona.nombreCompleto = "Casem Cotne";
+console.log(persona.nombre);                
+console.log(persona.apellido);               
+
+// Creamos un objeto que hereda de persona
+let estudiante = Object.create(persona);
+estudiante.curso = "Inglés";
+
+console.log(estudiante.nombreCompleto);      
+console.log(estudiante.curso);               
+
+let infoExtra = { universidad: "UMIN", graduado: false };
+Object.assign(estudiante, infoExtra);
+console.log(estudiante.universidad);         
+
+for (let prop in estudiante) {
+    if (estudiante.hasOwnProperty(prop)) {
+        console.log(`Propiedad propia: ${prop}`);
+    }
+}
